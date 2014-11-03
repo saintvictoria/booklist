54fe1c95733f05e17fbc67e997f292179a86607a
Victoria Nicholson <saintvictoria@gmail.com>
2014-11-02 20:15:23 -0500

    mostly working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
 
 var my_server = 'http://tiy-atl-fe-server.herokuapp.com/collections/vicbooks';
 
@@ -28,23 +31,45 @@ var Books = Backbone.Collection.extend({
 
 var allBooks = new Books();
 
-
-$('#addBook').on('submit', function(){
-
-  var $form = $(this);
-
-  var title = $form.find('#title').val();
-  var author = $form.find('#author').val();
-  var genre = $form.find('#genre').val();
-
-  var x = new Book({ 'title': title, 'author': author, 'genre': genre });
-  allBooks.add(x).save().done(function(){
-    $form[0].reset();
-  });
-
-
-
-
-  return  false;
-
+var Workspace = Backbone.Router.extend({
+  'routes': {
+    "add": 'addlist',
+    "": 'booksadded'
+  },
+  'addlist': function(){
+    console.log('welcome');
+    $('#books').show();
+  },
+  'booksadded': function(){
+    var source = $('#bookTemplate').html();
+    var template = Handlebars.compile(source);
+    allBooks.fetch().done(function(){
+      var data = {'books': allBooks.toJSON()};
+      $('#booklist > ul').html(template(data));
+      $('#booklist').show();
+    });
+  }
 });
+
+ var router = new Workspace();
+
+ $('#addBook').on('submit', function(){
+
+   var $form = $(this);
+
+   var title = $form.find('#title').val();
+   var author = $form.find('#author').val();
+   var genre = $form.find('#genre').val();
+
+   var x = new Book({ 'title': title, 'author': author, 'genre': genre });
+   allBooks.add(x).save().done(function(){
+     $form[0].reset();
+     router.navigate('', {trigger: true});
+   });
+
+   return  false;
+
+ });
+
+
+Backbone.history.start();
