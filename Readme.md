9b97238ace21945a9f2aed76cdc7066066298295
Victoria Nicholson <saintvictoria@gmail.com>
2014-11-09 19:55:43 -0500

    styling

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
+A simple backbone app for assignment 16.
+
+Mine is a library app to keep track of books you wnat t read.
+
+I used, backbone, JQuery and handlebars.
+
 ## Yo, I'm a Readme
 
 You should probably update me so people looking at your code have an idea what it is being used for :)
--- app/scripts/views/edit_book.js
+++ app/scripts/views/edit_book.js
@@ -3,7 +3,7 @@
   App.Views.EditBook = Backbone.View.extend({
 
     events: {
-      'submit #updateBook' : 'update',
+      'click #updateBook' : 'update',
       'click #delete' : 'deleteBook'
     },
 
@@ -36,9 +36,23 @@
         comments: $('#update_comments').val()
       });
 
-      this.options.book.save()
+      this.options.book.save(null, {
+        success: function(){
+          App.router.navigate('', {trigger: true});
+        }
+      });
 
       //Go back to our home page
+      App.router.navigate('', {trigger: true});
+
+    },
+    deleteBook: function (e) {
+      e.preventDefault();
+
+      // Remove Coffee
+      this.options.book.destroy();
+
+      // Go home ET
       App.router.navigate('', {trigger: true});
 
     }
